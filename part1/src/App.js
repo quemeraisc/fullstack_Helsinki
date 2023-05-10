1f26fa21a02bdfc2fac7b6f56fc6a601e86f3a21
quemeraisc <quemeraisc@debian.lab>
2023-05-10 09:53:02 +0100

    Step4 (1.4): use an array, but still using individual parts

--- part1/src/App.js
+++ part1/src/App.js
@@ -30,26 +30,28 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ]
 
   return (
     <div>
       <Header course = {course} />
-      <Content part = {part1.name} exercise = {part1.exercises} />
-      <Content part = {part2.name} exercise = {part2.exercises} />
-      <Content part = {part3.name} exercise = {part3.exercises} />
-      <Total total = {part1.exercises + part2.exercises + part3.exercises} />
+      <Content part = {parts[0].name} exercise = {parts[0].exercises} />
+      <Content part = {parts[1].name} exercise = {parts[1].exercises} />
+      <Content part = {parts[2].name} exercise = {parts[2].exercises} />
+      <Total total = {parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </div>
   )
 }
