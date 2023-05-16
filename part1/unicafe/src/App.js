2628a8d1ab8daf6035bd6c485ff93af302fad607
quemeraisc <quemeraisc@debian.lab>
2023-05-16 15:39:58 +0100

    Code cleaning before 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -18,42 +18,55 @@ const Stats = ({good,neutral,bad,total}) => {
           <h2>
             Statistics
           </h2>
-          <Stat
-            title = "good"
-            stat = {good}
-          />
-          <Stat
-              title = "neutral"
-              stat = {neutral}
-          />
-          <Stat
-              title = "bad"
-              stat = {bad}
-          />
-          <Stat
-              title = "All"
-              stat = {total}
-          />
-          <Stat
-              title = "Average"
-              stat = {(good - bad) / total}
-          />
-          <Stat
-              title = "Positive"
-              stat = {(good / total) * 100}
-          />
-
+          <StatisticLine title="good" stat={good} />
+          <StatisticLine title="neutral" stat={neutral} />
+          <StatisticLine title="bad" stat={bad} />
+          <StatisticLine title="All" stat={total} />
+          <StatisticLine title="Average" stat={(good - bad) / total} />
+          <StatisticLine title="Positive" stat={(good / total) * 100} />
       </div>
     )
 }
 
-const Stat = ({title, stat}) => {
+const StatisticLine = ({title, stat}) => {
     return (
       <div>
           <p>
               {title}: {stat}
           </p>
       </div>
+    )
+}
+
+const AppButton = ({value, text, setValue, setTotal, value2, value3}) => {
+    return (
+        <button onClick={() => {
+            const updatedValue = value + 1
+            setValue(updatedValue);
+            setTotal(updatedValue+value3+value3);
+            }
+        }>
+            {text}
+        </button>
+    )
+}
+
+const Buttons  = ({good, neutral, bad, setGood, setTotal, setNeutral, setBad}) => {
+    return (
+        <div>
+          <h2>
+            Give Feedback
+          </h2>
+          <AppButton
+            value={good} text="good" setValue={setGood} setTotal={setTotal} value2={neutral} value3={bad}
+          />
+          <AppButton
+             value={neutral} text="neutral" setValue={setNeutral} setTotal={setTotal} value2={good} value3={bad}
+          />
+          <AppButton
+             value={bad} text="bad" setValue={setBad} setTotal={setTotal} value2={neutral} value3={good}
+          />
+        </div>
     )
 }
 
@@ -64,47 +77,24 @@ const App = () => {
     const [bad, setBad] = useState(0)
     const [total, setTotal] = useState(0)
     
-    const percentPositve = () => {
-        if (total === 0) return 0
-        return (good / total) * 100
-    }
-
     return (
-        <div>
-            <h2>
-                Give Feedback
-            </h2>
-            <button onClick={() => {
-                const updatedGood = good + 1
-                setGood(updatedGood);
-                setTotal(updatedGood+neutral+bad);
-                }
-            }>
-                good
-            </button>
-            <button onClick={() => {
-                const updatedNeutral = neutral + 1
-                setNeutral(updatedNeutral);
-                setTotal(good+updatedNeutral+bad);
-            }
-            }>
-                neutral
-            </button>
-            <button onClick={() => {
-                const updatedBad = bad + 1
-                setBad(updatedBad);
-                setTotal(good+neutral+updatedBad);
-                }
-            }>
-                bad
-            </button>
-            <Stats
-                good = {good}
-                neutral = {neutral}
-                bad = {bad}
-                total = {total}
-            />
-        </div>
+      <div>
+        <Buttons
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          setGood={setGood}
+          setTotal={setTotal}
+          setNeutral={setNeutral}
+          setBad={setBad}
+          />
+        <Stats
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          />
+      </div>
     )
 }
 
