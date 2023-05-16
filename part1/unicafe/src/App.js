5c92c0f508a45900906254bc7d0a165351afea73
quemeraisc <quemeraisc@debian.lab>
2023-05-16 14:19:29 +0100

    1.8 better solution

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 
-const Stats = ({title, stat}) => {
+const Stats = () => {
+}
+
+const Stat = ({title, stat}) => {
     return (
       <div>
           <hr />
@@ -16,12 +19,12 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    let total = 0
+    const [total, setTotal] = useState(0)
     
-    const all = () => {
-        total =  good + neutral + bad
-        return total
-    }
+    // const all = () => {
+    //     total =  good + neutral + bad
+    //     return total
+    // }
     const average = () => {
         if (total === 0) return 0
         return (good - bad) / total
@@ -36,39 +39,54 @@ const App = () => {
             <h2>
                 Give Feedback
             </h2>
-            <button onClick={() => setGood(good + 1)}>
+            <button onClick={() => {
+                const updatedGood = good + 1
+                setGood(updatedGood);
+                setTotal(updatedGood+neutral+bad);
+                }
+            }>
                 good
             </button>
-            <button onClick={() => setNeutral(neutral + 1)}>
+            <button onClick={() => {
+                const updatedNeutral = neutral + 1
+                setNeutral(updatedNeutral);
+                setTotal(good+updatedNeutral+bad);
+            }
+            }>
                 neutral
             </button>
-            <button onClick={() => setBad(bad + 1)}>
+            <button onClick={() => {
+                const updatedBad = bad + 1
+                setBad(updatedBad);
+                setTotal(good+neutral+updatedBad);
+                }
+            }>
                 bad
             </button>
             <h2>
                 Statistics
             </h2>
-            <Stats
+            <Stat
                 title = "good"
                 stat = {good}
             />
-            <Stats
+            <Stat
                 title = "neutral"
                 stat = {neutral}
             />
-            <Stats
+            <Stat
                 title = "bad"
                 stat = {bad}
             />
-            <Stats
+            <Stat
                 title = "All"
-                stat = {all()}
+                stat = {total}
             />
-            <Stats
+            <Stat
                 title = "Average"
                 stat = {average()}
             />
-            <Stats
+            <Stat
                 title = "Positive"
                 stat = {percentPositve()}
             />
