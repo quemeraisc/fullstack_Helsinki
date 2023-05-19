63fd5ee8462f4fe7398bd5d5eba3b652000a0066
quemeraisc <quemeraisc@debian.lab>
2023-05-19 12:45:38 +0100

    1.13: anecdotes step2 completed

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,22 +1,7 @@
 import { useState } from 'react'
 
-const App = () => {
-    const anecdotes = [
-          'If it hurts, do it more often.',
-          'Adding manpower to a late software project makes it later!',
-          'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-          'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-          'Premature optimization is the root of all evil.',
-          'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-          'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-          'The only way to go fast, is to go well.'
-        ]
-     
-    const [selected, setSelected] = useState(0)
-
-    return (
-      <div>
-        {anecdotes[selected]}
+const NextButton = ({anecdotes, setSelected}) => {
+  return (
         <button onClick={() => {
           const rand = Math.floor(Math.random() * (anecdotes.length-1))
           console.log(rand)
@@ -25,8 +10,59 @@ const App = () => {
         }>
         next anecdote
         </button>
-      </div>
-    )
+  )
+}
+
+const VoteButton = ({selected, points, setPoints}) => {
+  return (
+    <button onClick={() => {
+      console.log(selected, 'voted')
+      console.log(points[selected], 'voted')
+      const copy  = [ ...points ]
+      copy[selected] += 1
+      setPoints(copy)
+      console.log(points[selected], 'voted')
+    }
+    }>
+      Vote
+    </button>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <br />
+      {points}
+      <br />
+      has {points[selected]} votes
+      <br />
+      <VoteButton
+        selected={selected}
+        points={points}
+        setPoints={setPoints}
+      />
+      <NextButton
+        anecdotes={anecdotes}
+        setSelected={setSelected}
+      />
+    </div>
+  )
 }
 
 export default App
